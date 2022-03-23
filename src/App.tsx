2822e5660f56c5c910a45e04163da2dd4361f84e
rfilano <rfilano6@gmail.com>
2022-03-23 16:44:26 -0400

    Moved Tasks C,D, and E into component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Counter } from "./Components/Counter";
+import { MultipleCounters } from "./Components/MultiCounters";
 
 function App(): JSX.Element {
     return (
@@ -9,7 +10,10 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <div>Welcome to Score Counter!</div>
+            <p>Task C and E</p>
             <Counter></Counter>
+            <p>Task D</p>
+            <MultipleCounters></MultipleCounters>
             <p>Team 17</p>
         </div>
     );
