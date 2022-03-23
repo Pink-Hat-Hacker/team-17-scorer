dfa487e8dbd2d9d0a12882b4bf7153e44d44341e
Pink-Hat-Hacker <zyv@udel.edu>
2022-03-23 17:54:31 -0400

    trying to solve issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,6 @@ import { Counter } from "./Components/Counter";
 import { MultipleCounters } from "./Components/MultiCounters";
 
 function App(): JSX.Element {
-    const [currentScore, setCurrentScore] = useState<number>(0);
-    const [state, setCounter] = useState({
-        counterA: 0,
-        counterB: 0,
-        counterC: 0
-    });
-
-    function decreaseScore() {
-        setCurrentScore(currentScore - 1);
-    }
-
-    function increaseScore() {
-        setCurrentScore(currentScore + 1);
-    }
-
     return (
         <div className="App">
             <header className="App-header">
