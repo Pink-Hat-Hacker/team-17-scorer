e71b736e0b5abdce16955c61d7a9d54075f16029
Sydney <sydneyph@udel.edu>
2022-03-23 11:26:26 -0400

    Finished the decrease button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [currentScore, setCurrentScore] = useState<number>(0);
+
+    function decreaseScore() {
+        setCurrentScore(currentScore - 1);
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Button onClick={decreaseScore}>Decrease Score</Button>
+            <div>{currentScore}</div>
         </div>
     );
 }
