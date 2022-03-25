8517e6e67f42105d72576aaefa061c590fe0df96
Sydney <sydneyph@udel.edu>
2022-03-25 11:50:45 -0400

    We have the delete buttons all set up. Could eb more efficient if we implemented them with a loop but all of the other buttons would also have to be loops. Left the original counter in tact but i can add a delete button to that if we want to as well.

--- src/Components/MultiCounters.tsx
+++ src/Components/MultiCounters.tsx
@@ -7,39 +7,66 @@ export function MultipleCounters(): JSX.Element {
         counterB: 0,
         counterC: 0
     });
+    const [visibleButton1, setVisibility1] = useState<boolean>(true);
+    const [visibleButton2, setVisibility2] = useState<boolean>(true);
+    const [visibleButton3, setVisibility3] = useState<boolean>(true);
 
     return (
         <div>
-            <div>
-                <Button
-                    onClick={() =>
-                        setCounter({ ...state, counterA: state.counterA + 1 })
-                    }
-                >
-                    <span>Custom A</span>
-                </Button>
-                <div>Counter = {state.counterA}</div>
-            </div>
-            <div>
-                <Button
-                    onClick={() =>
-                        setCounter({ ...state, counterB: state.counterB + 1 })
-                    }
-                >
-                    <span>Custom B</span>
-                </Button>
-                <div>Counter = {state.counterB}</div>
-            </div>
-            <div>
-                <Button
-                    onClick={() =>
-                        setCounter({ ...state, counterC: state.counterC + 1 })
-                    }
-                >
-                    <span>Custom C</span>
-                </Button>
-                <div>Counter = {state.counterC}</div>
-            </div>
+            {visibleButton1 && (
+                <div>
+                    <Button
+                        onClick={() =>
+                            setCounter({
+                                ...state,
+                                counterA: state.counterA + 1
+                            })
+                        }
+                    >
+                        <span>Custom A</span>
+                    </Button>
+                    <Button onClick={() => setVisibility1(false)}>
+                        Delete
+                    </Button>
+                    <div>Counter = {state.counterA}</div>
+                </div>
+            )}
+            {visibleButton2 && (
+                <div>
+                    <Button
+                        onClick={() =>
+                            setCounter({
+                                ...state,
+                                counterB: state.counterB + 1
+                            })
+                        }
+                    >
+                        <span>Custom B</span>
+                    </Button>
+                    <Button onClick={() => setVisibility2(false)}>
+                        Delete
+                    </Button>
+                    <div>Counter = {state.counterB}</div>
+                </div>
+            )}
+            {visibleButton3 && (
+                <div>
+                    <Button
+                        onClick={() =>
+                            setCounter({
+                                ...state,
+                                counterC: state.counterC + 1
+                            })
+                        }
+                    >
+                        <span>Custom C</span>
+                    </Button>
+                    <Button onClick={() => setVisibility3(false)}>
+                        Delete
+                    </Button>
+                    <div>Counter = {state.counterC}</div>
+                </div>
+            )}
         </div>
     );
 }
